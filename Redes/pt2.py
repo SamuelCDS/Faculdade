a7d7d916e318825c554b42ed303f667e5a18919e
SamuelCDS <93876483+SamuelCDS@users.noreply.github.com>
2023-06-10 16:08:11 -0300

    Delete pt2.py

--- Redes/pt2.py
+++
@@ -1,7 +0,0 @@
-import http.client
-conn = http.client.HTTPConnection(HOST, PORT)
-conn.request("GET" "/pages/index2.html")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-headers = r1.getheader()
-print(headers[0][0], ":", headers[0][1])
