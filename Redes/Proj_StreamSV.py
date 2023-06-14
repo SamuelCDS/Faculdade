e35956068f56df9da0f19f1575f9385527b5ad3e
SamuelCDS <93876483+SamuelCDS@users.noreply.github.com>
2023-06-14 03:56:10 -0300

    Update Proj_StreamSV.py

--- Redes/Proj_StreamSV.py
+++ Redes/Proj_StreamSV.py
@@ -5,83 +5,119 @@ class Server:
         self.host = host
         self.port = port
         self.server = None
-        self.stream = None
-        self.musica = None
-    
-    def playa(self, event: threading.Event):
-        #Envio da musica:
-        arquivo = self.musica.readframes(1024)
-        while arquivo:
-            self.stream.write(arquivo)
-            if not arquivo or event.is_set():
-                while True:
-                    if not event.is_set():
-                        break
-            arquivo = self.musica.readframes(1024)
-        self.stream.stop_stream()
-        self.stream.close()
-        self.audio.terminate()
-        self.client.close()
-
-    def initMusic(self):
-        #recebimento da musica desejada:
-        self.selecao_encoded = self.client.recv(1024)
-        self.SelecM = self.selecao_encoded.decode()
-        print("Música selecionada.")
-
-        #Inicialização do PyAudio:
         self.audio = pyaudio.PyAudio()
-        direc = os.path.join("/home/samuelcds/Músicas", self.SelecM)
-        self.musica = wave.open(direc, 'rb')
-        self.stream = self.audio.open(format=self.audio.get_format_from_width(self.musica.getsampwidth()),
-                                 channels=self.musica.getnchannels(),
-                                 rate=self.musica.getframerate(),
-                                 output=True,
-                                 frames_per_buffer=1024)
+
+    def EachCmdfor(self, clien):
+        stream = None
+        musica = None
+        event = threading.Event()
+        tocarM = None
+        
+        #Envio da lista de músicas:
+        clien.sendall('\n'.join(os.listdir("/home/samuelcds/Músicas")).encode())
+
+        #Processamento geral do servidor:
+        try:
+
+            def playa(event: threading.Event):
+                #Envio da musica:
+                while True:
+                    if musica is None:
+                        return
+                    arquivo = musica.readframes(1024)
+                    stream.write(arquivo)
+                    if not arquivo or event.is_set():
+                        while True:
+                            if not event.is_set():
+                                break
+            
+            #Recebimento da musica desejada:
+            selecao_encoded = clien.recv(1024)
+            SelecM = selecao_encoded.decode()
+            print(f"Música selecionada: {SelecM}")
+
+            #Inicialização do PyAudio:
+            direc = os.path.join("/home/samuelcds/Músicas", SelecM)
+            musica = wave.open(direc, 'rb')
+            stream = self.audio.open(format=self.audio.get_format_from_width(musica.getsampwidth()),
+                                        channels=musica.getnchannels(),
+                                            rate=musica.getframerate(),
+                                          output=True,
+                                        frames_per_buffer=1024,
+                                        output_device_index=self.audio.get_default_output_device_info()['index'])
+
+            #Recebimento de comandos:
+            while True:
+                try:
+                    cmdMusica = clien.recv(1024).decode()
+                except ConnectionResetError:
+                    print("Conexão perdida.")
+                    break
+                except Exception as e:
+                    stream.stop_stream()
+                    stream.close()
+                    musica.close()
+                    print(f"Erro durante a comunicação com o cliente: {str(e)}")
+                    break
+
+                if cmdMusica == "Play":
+                    print("Comando de reprodução recebido.")
+                    if tocarM is None or not tocarM.is_alive():
+                        event.clear()
+                        tocarM = threading.Thread(target=playa, args=(event,))
+                        tocarM.start()
+                    else:
+                        event.set()
+                        event.clear()
+
+                elif cmdMusica == "Pause":
+                    print("comando de pausa recebido.")
+                    event.set()
+
+                elif cmdMusica == "Restart":
+                    print("Comando para reiniciar recebido.")
+                    event.clear()
+                    musica.rewind()
+                    stream.start_stream()
+            
+                else:
+                    event.set()
+                    print(f"Comando para mudar música recebido: {cmdMusica}")
+                    stream.stop_stream()
+                    stream.close()
+                    musica.close()
+                    event.clear()
+                    direc = os.path.join("/home/samuelcds/Músicas", cmdMusica)
+                    musica = wave.open(direc, 'rb')
+                    stream.start_stream()
+
+        except ConnectionResetError:
+            print("Conexão perdida.")
+        except Exception as e:
+            print(f"Erro durante a comunicação com o cliente: {str(e)}")
+
+        finally:
+            if stream:
+                stream.stop_stream()
+                stream.close()
+            if self.audio:
+                self.audio.terminate()
+            if musica:
+                musica.close()
+            
     
     def start(self):
         #Iniciando servidor:
-        event = threading.Event()
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.host, self.port))
         self.server.listen(4)
         print('Servidor iniciado, aguardando conexões...')
-        tocarM = threading.Thread(target=self.playa, args=(event,))
-        self.client, address_cl = self.server.accept()
-        print('Cliente: ', address_cl, 'CONECTADO')
-
-        #Envio da lista de músicas:
-        self.client.sendall('\n'.join(os.listdir("/home/samuelcds/Músicas")).encode())
-
-        #Primeira inicialização do PyAudio:
-        self.initMusic()
-
-        #Recebimento de comandos:
+        
         while True:
-            try:
-                cmdMusica = self.client.recv(1024).decode()
-            except Exception:
-                self.stream.stop_stream()
-                self.stream.close()
-                self.audio.terminate()
-                self.client.close()
-
-            if cmdMusica == "play":
-                print("Comando de reprodução recebido.")
-                try:
-                    tocarM.start()
-                except:
-                    event.clear()
-
-            elif cmdMusica == "Pause":
-                print("comando de pausa recebido.")
-                event.set()
-
-            elif cmdMusica == "Restart":
-                print("Comando para reiniciar recebido.")
-            else:
-                self.initMusic()
-
+            client, address_cl = self.server.accept()
+            print('Cliente: ', address_cl, 'CONECTADO')
+            eachCon = threading.Thread(target=self.EachCmdfor, args=(client,))
+            eachCon.start()
                 
 if __name__ == '__main__':
     server = Server('localhost', 1234)
